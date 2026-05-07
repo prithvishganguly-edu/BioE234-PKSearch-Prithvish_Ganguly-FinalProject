e87bd017550651a52d16b537aff37c09eed9eedd
Prithvish Ganguly <prithvish.ganguly@gmail.com>
2026-05-06 22:37:37 -0700

    Add README.md and THEORY.md for search_pks tool

--- modules/seq_basics/resources/__init__.py
+++
@@ -1,4 +0,0 @@
-"""
-Package marker for the resources/ sub-package.
-Students: you do not need to edit this file.
-"""
--- modules/seq_basics/tools/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Package marker for the tools/ sub-package.
-Required so that register.py can import tools as
-    modules.seq_basics.tools.<tool_name>
- 
-Students: you do not need to edit this file.
-To add a tool, just drop a new .py file in this folder.
-"""
